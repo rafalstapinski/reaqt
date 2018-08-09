37f796386db77e5d8476a047101a7180b64d29dc
rafalstapinski <stapinskirafal@gmail.com>
2018-08-09 16:42:52 -0400

    initial commit

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const Home: React.SFC = () => {
+  return <p>{'HOME PAGE'}</p>
+}
+
+export default Home
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const NotFound: React.SFC = () => {
+  return <p>{'Not Found!!!!'}</p>
+}
+
+export default NotFound
